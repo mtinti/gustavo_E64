b8f5f57f7dde0f464e003d696050ad7e273bd741
mtinti <michele.tinti@gmail.com>
2022-02-06 08:58:16 +0000

    start

---
+++ config.js
@@ -0,0 +1,29 @@
+let X1='logFC';
+let Y1='FDR';
+let X2='log_AveExpr';
+let Y2='logFC';
+
+let FLIP_Y1=true;
+let FLIP_Y2=false;
+
+let PLOT_WIDTH=550;
+let PLOT_HEIGHT=400;
+
+let PROJECT_TITLE='MKT1';
+
+let margin = 0.05;
+
+let BOX_PLOT_COLS_1 = ['64P1','64P2','64P3']
+let BOX_PLOT_COLS_2 = ['64M1','64M2','64M3']
+
+let CONDITION_1 = 'Plus'
+let CONDITION_2 = 'Minus'
+
+
+let COLOR_SELECTION_TABLE = 'orange';
+let COLOR_SELECTION_CIRCLE = 'orange';
+
+let Y_LABEL_BARPLOT = 'Intensity';
+
+let File_1 = 'indata_64.csv';
+let File_2 = 'indata2_64.csv';
